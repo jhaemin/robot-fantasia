1b31c8db9679ee959992823ec3e4e250f46bcf05
Jang Haemin <io@jhaemin.com>
2023-12-07 01:21:40 +0900

    Update open graph

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,14 @@ import './page.scss'
 export const metadata: Metadata = {
   title: '로봇 판타지아',
   description: '배민 음악 로봇 판타지아',
+  openGraph: {
+    type: 'website',
+    images: ['https://robotfantasia.com/og_image.png'],
+    title: '로봇 판타지아',
+    description: '배민 음악 로봇 판타지아',
+    locale: 'ko',
+    siteName: '로봇 판타지아',
+  },
 }
 
 export default function RootLayout({
