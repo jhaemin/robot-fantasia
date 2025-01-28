a9d1be1851ae3b26fddf4eaa9fe3dc961d777698
Jang Haemin <io@jhaemin.com>
2025-01-28 11:17:16 +0900

    Add favicon

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -23,6 +23,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="ko-KR">
+      <head>
+        <link
+          rel="icon"
+          type="image/png"
+          href="/favicon-96x96.png"
+          sizes="96x96"
+        />
+        <link rel="icon" type="image/svg+xml" href="/favicon.svg" />
+        <link rel="shortcut icon" href="/favicon.ico" />
+        <link
+          rel="apple-touch-icon"
+          sizes="180x180"
+          href="/apple-touch-icon.png"
+        />
+        <link rel="manifest" href="/site.webmanifest" />
+      </head>
       <body>{children}</body>
     </html>
   )
